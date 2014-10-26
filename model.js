5287e56dca93dd333d43bc90c077099120d653ae
Itay Maman <imaman@google.com>
2014-10-26 20:10:06 +0200

    arr -> data

--- model.js
+++ model.js
@@ -8,37 +8,37 @@ exports.newModel = function() {
     return res;
   }
 
-  var arr = [
+  var data = [
     { id: nextId(), text: 'Create a TodoMVC template', completed: true },
     { id: nextId(), text: 'Rule the web', completed: false },
   ];
 
   return {
-    add: function(obj) { obj.id = nextId(); arr.push(obj); },
+    add: function(obj) { obj.id = nextId(); data.push(obj); },
     removeAll: function(pred) {
-      arr = arr.filter(function(curr) { return !pred(curr); });
+      data = data.filter(function(curr) { return !pred(curr); });
     },
-    forEach: function(f) { arr.forEach(f); },
+    forEach: function(f) { data.forEach(f); },
     lookup: function(id) {
-      var res = arr.filter(function(curr) { return curr.id == id });
+      var res = data.filter(function(curr) { return curr.id == id });
       if (res.length != 1)
         throw new Error('Lookup of ID ' + id + ' has failed');
       return res[0];
     },
     remove: function(id) {
-      var positions = arr.map(function(curr, pos) {
+      var positions = data.map(function(curr, pos) {
         return curr.id == id ? pos : -1
       });
       positions = positions.filter(function(pos) { return pos >= 0; });
       if (positions.length != 1)
         throw new Error('Lookup of ID ' + id + ' has failed');
 
-      arr.splice(positions[0], 1);
+      data.splice(positions[0], 1);
     },
     findAll: function(pred) {
-      return arr.filter(pred || function() { return true; });
+      return data.filter(pred || function() { return true; });
     },
-    size: function() { return arr.length; }
+    size: function() { return data.length; }
   };
 };
 
