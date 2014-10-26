e4146dc961903cda30187dda5703e4f821f62ce8
Itay Maman <imaman@google.com>
2014-10-27 00:44:51 +0200

    introduce one()

--- model.js
+++ model.js
@@ -23,9 +23,10 @@ exports.newModel = function() {
     return {
       forEach: function(act) { asArr().forEach(act) },
       map: function(f) { return asArr().map(f) },
-      get: function() { return data[id] || null },
+      get: function() { return asArr() },
       remove: function() { delete data[id] },
-      size: function() { return asArr().length }
+      size: function() { return asArr().length },
+      one: function() { return data[id] || null }
     };
   }
 
@@ -36,14 +37,14 @@ exports.newModel = function() {
       map: function(f) { return this.get().map(f) },
       remove: function() { this.forEach(function(curr) { delete data[curr.id] }) },
       get: function() { return all().filter(pred) },
-      size: function() { return this.get().length }
+      size: function() { return this.get().length },
     };
   }
 
   return {
     q: function(arg) { return (arg === null || arg === undefined) ? selectAll() : typeof(arg) === 'function' ? selectAll(arg) : select(arg) },
     add: function(obj) { obj.id = nextId(); data[obj.id] = obj; return obj.id; },
-    at: function(id) { return this.q(id).get() },
+    at: function(id) { return this.q(id).one() },
     size: function() { return this.q().size() },
     toString: function() { return JSON.stringify(data, null, 2); }
   };
