494f5625fa08cc58c8c359324d303d71199b1294
Itay Maman <imaman@google.com>
2014-10-26 21:40:23 +0200

    introduce select(), selectAll()

--- model.js
+++ model.js
@@ -14,19 +14,29 @@ exports.newModel = function() {
     return Object.keys(data).filter(function(key) { return key }).map(function(key) { return data[key]; });
   }
 
+  function select(id) {
+    return {
+      get: function() { return data[id] || null },
+      remove: function() { data[id] = null }
+    };
+  }
+
+  function selectAll(pred) {
+    return {
+      forEach: function(act) { all().filter(pred).forEach(act) },
+      remove: function() { this.forEach(function(curr) { delete data[curr.id] }) }
+    };
+  }
+
   return {
     add: function(obj) { obj.id = nextId(); data[obj.id] = obj; return obj.id; },
-    removeAll: function(pred) {
-      all().filter(function(curr) { return pred(curr); }).forEach(function(curr) {
-        delete data[curr.id];
-      });
-    },
+    removeAll: function(pred) { selectAll(pred).remove(); },
     forEach: function(f) { all().forEach(f); },
     lookup: function(id) {
-      return data[id] || null;
+      return select(id).get();
     },
     remove: function(id) {
-      delete data[id];
+      select(id).remove();
     },
     findAll: function(pred) {
       return all().filter(pred || function() { return true; });
