f9aaee61029bdce6320c853c5f2f2966dfde5b34
Itay Maman <imaman@google.com>
2014-10-30 00:49:56 +0200

    extract auto_controller.js

---
+++ auto_controller.js
@@ -0,0 +1,45 @@
+function entityDelete(q, res) {
+  q.remove();
+  res.sendStatus(200).end();
+}
+
+function newDeleteController(selection, idParam) {
+  if (!idParam) {
+    return function(req, res) {
+      entityDelete(selection, res);
+    };
+  }
+
+  return function(req, res) {
+    entityDelete(selection.q(req.params[idParam]), res);
+  };
+}
+
+exports.create = function(name, selection, idParam) {
+  return {
+    post: function(jsonFromReq) {
+      return function(req, res) {
+        selection.add(jsonFromReq(req));
+        res.sendStatus(200).end();
+      };
+    },
+    delete: function() { return newDeleteController(selection, idParam) },
+    get: function(jsonFromReq) {
+      return function(req, res) {
+        var data = jsonFromReq(req, selection.q(req.params[idParam]));
+        data.byController = data.byController || name;
+        res.render('index', data);
+      };
+    },
+    put: function() {
+      return function(req, res) {
+        var newState = (req.param('completed') === 'true');
+        selection.q(req.params[idParam]).forEach(function(curr) {
+          curr.completed = newState;
+        });
+        res.sendStatus(200).end();
+      };
+    }
+  }
+}
+
--- controller.js
+++ controller.js
@@ -1,60 +1,17 @@
+var autoController = require('./auto_controller.js');
 
 function install(model, app) {
   app.get('/', function(req, res) {
     res.redirect('/todos');
   });
 
-  function entityDelete(q, res) {
-    q.remove();
-    res.sendStatus(200).end();
-  }
-  function newDeleteController(selection, idParam) {
-    if (!idParam) {
-      return function(req, res) {
-        entityDelete(selection, res);
-      };
-    }
-
-    return function(req, res) {
-      entityDelete(selection.q(req.params[idParam]), res);
-    };
-  }
-
-  function newController(name, selection, idParam) {
-    return {
-      post: function(jsonFromReq) {
-        return function(req, res) {
-          model.add(jsonFromReq(req));
-          res.sendStatus(200).end();
-        };
-      },
-      delete: function() { return newDeleteController(selection, idParam) },
-      get: function(jsonFromReq) {
-        return function(req, res) {
-          var data = jsonFromReq(req, selection.q(req.params[idParam]));
-          data.byController = data.byController || name;
-          res.render('index', data);
-        };
-      },
-      put: function() {
-        return function(req, res) {
-          var newState = (req.param('completed') === 'true');
-          selection.q(req.params[idParam]).forEach(function(curr) {
-            curr.completed = newState;
-          });
-          res.sendStatus(200).end();
-        };
-      }
-    }
-  }
-
   var completed = model.q(function(e) { return e.completed });
   var active = model.q(function(e) { return !e.completed });
 
-  var completedTodosController = newController('todo_completed', completed);
-  var activeTodosController = newController('todo_active', active);
-  var todoController = newController(null, model, 'id');
-  var todosContoller = newController('todo', model);
+  var completedTodosController = autoController.create('todo_completed', completed);
+  var activeTodosController = autoController.create('todo_active', active);
+  var todoController = autoController.create(null, model, 'id');
+  var todosContoller = autoController.create('todo', model);
 
   app.delete('/todos_completed', completedTodosController.delete());
   app.delete('/todos/:id', todoController.delete());
