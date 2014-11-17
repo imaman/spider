a3a2bec469448e8e9ab2c2071a0cf60d67e738eb
Itay Maman <imaman@google.com>
2014-11-17 19:11:06 +0200

    extract methods in model.js

--- src/framework/model.js
+++ src/framework/model.js
@@ -14,45 +14,45 @@ function create(coll) {
     });
   }
 
-  function pick(where) {
-    if (typeof where === 'string') {
-      var id = ObjectID.createFromHexString(where);
-      var byId = {_id: id};
-      return {
-        remove: function(done) {
-          coll.removeOne(byId, done);
-        },
-        map: function(mapper, done) {
-          coll.find(byId).toArray(function(err, data) {
-            if (err) return done(err);
-            done(null, data.map(mapper));
-          });
-        },
-        size: function(done) {
-          coll.find(byId).toArray(function(err, data) {
-            if (err) return done(err);
-            done(null, data.length);
-          });
-        },
-        one: function(done) {
-          coll.find(byId).toArray(function(err, data) {
-            if (err) return done(err);
-            var len = data.length;
-            if (len > 1) return done('more than one.');
-            if (len == 0) return done(null, null);
-            done(null, data[0]);
-          });
-        },
-        update: function(change, done) {
-          normalizeChange(change);
-          coll.update(byId, {$set: change}, done);
-        },
-        get: function(done) {
-          return this.one(done);
-        }
-      };
-    }
-    where = where || {};
+  function elementQuery(where) {
+    var id = ObjectID.createFromHexString(where);
+    var byId = {_id: id};
+    return {
+      remove: function(done) {
+        coll.removeOne(byId, done);
+      },
+      map: function(mapper, done) {
+        coll.find(byId).toArray(function(err, data) {
+          if (err) return done(err);
+          done(null, data.map(mapper));
+        });
+      },
+      size: function(done) {
+        coll.find(byId).toArray(function(err, data) {
+          if (err) return done(err);
+          done(null, data.length);
+        });
+      },
+      one: function(done) {
+        coll.find(byId).toArray(function(err, data) {
+          if (err) return done(err);
+          var len = data.length;
+          if (len > 1) return done('more than one.');
+          if (len == 0) return done(null, null);
+          done(null, data[0]);
+        });
+      },
+      update: function(change, done) {
+        normalizeChange(change);
+        coll.update(byId, {$set: change}, done);
+      },
+      get: function(done) {
+        return this.one(done);
+      }
+    };
+  }
+
+  function collectionQuery(where) {
     return {
       map: function(mapper, done) {
         coll.find(where).toArray(function(err, data) {
@@ -90,33 +90,44 @@ function create(coll) {
     }
   }
 
-  return {
-    size: function(done) {
-      coll.count({}, done);
-    },
-    add: function() {
-      var args = Array.prototype.slice.call(arguments, 0);
-      var done = args.pop();
-      coll.insertMany(args, function(err, writeOpData) {
-        if (err) return done(err);
-        var ids = writeOpData.ops.map(function(curr) { return curr._id.toHexString() });
-        done.bind(null, null).apply(null, ids);
-      });
-    },
-    q: function(where) {
-      return pick(where);
-    },
-    at: function(id, done) {
-      coll.findOne({_id: ObjectID.createFromHexString(id)}, function(err, data) {
-        if (err) return done(err);
-        if (!data) return done(null, data);
-        data._id = data._id.toHexString();
-        done(null, data);
-      });
-    },
-    newModel: function(coll) {
-      return create(coll);
-    }
-  };
+  function rootQuery() {
+    return {
+      size: function(done) {
+        coll.count({}, done);
+      },
+      add: function() {
+        var args = Array.prototype.slice.call(arguments, 0);
+        var done = args.pop();
+        coll.insertMany(args, function(err, writeOpData) {
+          if (err) return done(err);
+          var ids = writeOpData.ops.map(function(curr) { return curr._id.toHexString() });
+          done.bind(null, null).apply(null, ids);
+        });
+      },
+      q: function(where) {
+        return pick(where);
+      },
+      at: function(id, done) {
+        coll.findOne({_id: ObjectID.createFromHexString(id)}, function(err, data) {
+          if (err) return done(err);
+          if (!data) return done(null, data);
+          data._id = data._id.toHexString();
+          done(null, data);
+        });
+      },
+      newModel: function(coll) {
+        return create(coll);
+      }
+    };
+  }
+
+  function pick(where) {
+    if (typeof where === 'string')
+      return elementQuery(where);
+    else
+      return collectionQuery(where || {});
+  }
+
+  return rootQuery();
 }
 exports.newModel = create;
